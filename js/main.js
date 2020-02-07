6afbadd7e888bc55f2daa5772c033789a2484335
StraightEdgeSxE <60220767+StraightEdgeSxE@users.noreply.github.com>
2020-02-07 16:26:18 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,20 @@
+var mySwiper = new Swiper('.swiper-container', {
+    slidesPerView: 2,
+    loop: true,
+    navigation: {
+        nextEl:'.arrow' ,
+    },
+    breakpointsa: {
+        540: {
+            slidesPerView:1
+        }
+    }
+});
+
+
+var menuButton = document.querySelector('.menu-button');
+var menu= document.querySelector('.header');
+menuButton.addEventListener('click' , function(){
+    menuButton.classList.toggle ('menu-button-active');
+    menu.classList.toggle ('header-active');
+})
